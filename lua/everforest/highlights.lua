7c2ac6a2e728949b7076933c9a2f6606212f0fa4
fys <fengys1996@gmail.com>
2025-02-25 22:01:17 +0800

    chore: make font thin

--- lua/everforest/highlights.lua
+++ lua/everforest/highlights.lua
@@ -4,7 +4,7 @@ local ColourUtility = require("everforest.colour_utility")
 
 ---@enum Styles
 local styles = {
-  bold = "bold",
+  bold = "",
   italic = "italic",
   reverse = "reverse",
   undercurl = "undercurl",
