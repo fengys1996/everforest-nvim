53834d5751dc75e9e9e295edfadc52e2d8336a6f
fys <fengys1996@gmail.com>
2025-02-25 22:10:38 +0800

    wip

--- lua/everforest/highlights.lua
+++ lua/everforest/highlights.lua
@@ -4,7 +4,7 @@ local ColourUtility = require("everforest.colour_utility")
 
 ---@enum Styles
 local styles = {
-  bold = "None",
+  bold = "normal",
   italic = "italic",
   reverse = "reverse",
   undercurl = "undercurl",
